c359bf788d0ab9ecdbce3b86d52a3585be3bb3a4
eigger <eigger87@gmail.com>
2026-06-23 13:18:07 +0900

    Fire hassble_ws_bridge_loaded event on setup (#17)
    * Fire hassble_ws_bridge_loaded event on setup
    
    * Bump version to 0.1.17

--- custom_components/ws_bridge/__init__.py
+++ custom_components/ws_bridge/__init__.py
@@ -47,6 +47,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
+
+    # Fire event to notify that ws_bridge has loaded/reloaded
+    hass.bus.async_fire("hassble_ws_bridge_loaded")
+
     return True
 
 
